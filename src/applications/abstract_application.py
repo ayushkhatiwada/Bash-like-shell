4f406bcf4fd695ea51727c2b6b6db6682887c0a7
ayush <ayush7khatiwada@gmail.com>
2023-11-12 16:05:10 +0000

    added abstract_application class, will use dynamic dispatch soon

---
+++ src/applications/abstract_application.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractApplication(ABC):
+    """
+    Abstract Application class
+
+    contains abstract exec function
+    each command class (pwd, ls, cat etc.) will inherit Application class and the abstract exec function 
+    each child class will overide the abstract exec function
+
+    Dynamic dispatch will then be used
+    """
+
+
+    @abstractmethod
+    def exec():
+        pass

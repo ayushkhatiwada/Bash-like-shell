676ff58d5b8e7ec1c92ad8274cb63cf4b7585ed8
ayush <ayush7khatiwada@gmail.com>
2023-11-13 16:14:28 +0000

    fixed type hints

--- src/applications/pwd.py
+++ src/applications/pwd.py
@@ -1,6 +1,7 @@
 import os
 from collections import deque
-from typing import Deque
+# from typing import Deque
+# Use deque directly from collections instead of using Deque from typing library - See PEP 585 https://peps.python.org/pep-0585/
 
 from abstract_application import AbstractApplication
 
@@ -11,6 +12,6 @@ class Pwd(AbstractApplication):
     """
 
     # out seems to be a double ended queue containing the current output
-    def exec(self, out: Deque[str]) -> None:
+    def exec(self, out: deque[str]) -> None:
         current_directory = os.getcwd()
         out.append(current_directory)

8e8b601193dca2fa62ed5db76343639b838eab8c
ayush <ayush7khatiwada@gmail.com>
2023-11-16 14:46:24 +0000

    modified some commands

--- src/applications/echo.py
+++ src/applications/echo.py
@@ -1,8 +1,12 @@
-from abstract_application import AbstractApplication
 from collections import deque
+
+from abstract_application import AbstractApplication
 
 
 class Echo(AbstractApplication):
     
     def exec(self, args: list[str], input: list[str], out: deque[str]) -> None:       
-        pass
+        output = " ".join(args)
+        out.append(output + "\n")
+
+# To may do: add flag -n to omit the new line

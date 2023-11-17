99524ab25586a308c50ed78cd6f436e560b39241
ayush <ayush7khatiwada@gmail.com>
2023-11-17 17:53:23 +0000

    added main_shell.py, this is where code will start, will rename to shell.py later

---
+++ src/main_shell.py
@@ -0,0 +1,25 @@
+"""
+This file will replace shell.py
+"""
+
+import os
+import sys
+
+from collections import deque
+
+
+def exec_shell():
+    num_of_args = len(sys.argv) - 1
+
+    # non interactive mode
+    if num_of_args > 0:
+        pass
+
+    # interactive mode
+    else:
+        pass
+
+
+
+if __name__ == "__main__":
+    exec_shell()

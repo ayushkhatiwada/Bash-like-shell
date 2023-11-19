8bc6590826c6581d8c22445688dbf32a62054f57
ayush <ayush7khatiwada@gmail.com>
2023-11-19 10:40:58 +0000

    started application factory

---
+++ src/application_factory.py
@@ -0,0 +1,19 @@
+from applications.cat import Cat
+from applications.cd import Cd
+from applications.cut import Cut
+from applications.echo import Echo
+from applications.find import Find
+from applications.grep import Grep
+from applications.head import Head
+from applications.ls import Ls
+from applications.pwd import Pwd
+from applications.sort import Sort
+from applications.uniq import Uniq
+from applications.tail import Tail
+
+
+class ApplicatoinFactory:
+
+
+    
+    pass

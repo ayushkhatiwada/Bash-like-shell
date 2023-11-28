6a6ba126dd3f234ae7470d70e2b9b0b925923a10
ayush <ayush7khatiwada@gmail.com>
2023-11-28 13:38:13 +0000

    first draft of expressions.py done

--- src/expressions.py
+++ src/expressions.py
@@ -1,73 +1,72 @@
 from visitor import Visitor
 
 
-# change accept commands to execute???
 class AbstractCommand:
     def __init__(self) -> None:
         pass
 
 
-# I don't think it should be called command
-# Should be called a command of commands - or a better name entirely
-# This is a non base case command
 class Commmand(AbstractCommand):
 
-    def accept(self, visitor):
+    def accept(self, visitor: Visitor):
         return visitor.visit_command(self)
 
 
 class Pipe(AbstractCommand):
-    # check type hints with HJP/XLow
-    def __init__(self, left_side, right_side: Commmand):
-        self.left_side = left_side
-        self.right_side = right_side
 
     def accept(self, visitor: Visitor):
-        return visitor.visit_pipe(self) 
+        return visitor.visit_pipe(self)
 
 
 class Seq(AbstractCommand):
 
-    def accept(self, visitor):
+    def accept(self, visitor: Visitor):
         return visitor.visit_seq(self)
 
 
-# I think this is the base case
-# Call represents ONE specific command (cat) and it's, one or more, input 
-# arguments (file1.txt, file2.txt)
 class Call(AbstractCommand):
 
-    def accept(self, visitor):
+    def accept(self, visitor: Visitor):
         return visitor.visit_call(self)
 
 
 class Atom:
-    pass
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_atom(self)
 
 
 class Argument:
 
-    def accept(self, visitor):
+    def accept(self, visitor: Visitor):
         return visitor.visit_argument(self)
 
 
 class Redirection:
-    pass
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_redirection(self)
 
 
 class Quoted:
-    pass
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_quoted(self)
 
 
 class SingleQuoted:
-    pass
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_single_quoted(self)
 
 
 class BackQuoted:
-    pass
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_back_quoted(self)
 
 
 class DoubleQuoted:
-    pass
 
-# Note: not sure how to handle redirection or command substitution
+    def accept(self, visitor: Visitor):
+        return visitor.visit_double_quoted(self)

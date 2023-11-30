e1a8ad6c8c4b1be3291988037e1cbcbd21bb7f11
Jan Pytel <jas.pytel@gmail.com>
2023-11-30 15:06:07 +0000

    Properly ls and test_ls

--- src/applications/ls.py
+++ src/applications/ls.py
@@ -1,17 +1,18 @@
 import os
-from collections import deque
+from typing import Deque, List
 
-from abstract_application import AbstractApplication
+from .application import Application, ApplicationError
 
 
-class Ls(AbstractApplication):
-    
-    name = "ls"
+class Ls(Application):
+    name = 'ls'
 
-    def __init__(self) -> None:
-        super().__init__()
-
-    def exec(self, args: list[str], input: list[str], out: deque[str]) -> None:
+    def exec(
+        self,
+        args: List[str],
+        input: List[str],
+        output: Deque[str]
+    ) -> None:
         # Determine the directory to list
         directory_path = args[0] if args else os.getcwd()
 
@@ -20,19 +21,19 @@ class Ls(AbstractApplication):
             items = os.listdir(directory_path)
 
             # Filter out items that start with '.'
-            visible_items = [item for item in items if not item.startswith('.')]
+            visible_items = [
+                item for item in items if not item.startswith('.')
+            ]
 
             # Format the output
             formatted_output = '\t'.join(visible_items) + '\n'
 
             # Add the formatted output to the deque
-            out.append(formatted_output)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"The directory {directory_path} does not exist.")
-        except PermissionError:
-            raise PermissionError(f"Permission denied to access: {directory_path}.")
-        except NotADirectoryError:
-            raise NotADirectoryError(f"{directory_path} is not a directory.")
+            output.append(formatted_output)
+        except (FileNotFoundError, NotADirectoryError):
+            raise ApplicationError(
+                f"{self.name}: {directory_path}: No such file or directory."
+            )
         except Exception as e:
             # General exception catch for any other unforeseen errors
-            raise Exception(f"An error occurred: {e}.")
+            raise ApplicationError(f"An error occurred: {e}.")

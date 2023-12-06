07d684d8b264e3113e4313aa7687d6461c2ca902
Maximus <maxabela@gmail.com>
2023-12-06 09:25:22 +0000

    updating sort

--- src/applications/sort.py
+++ src/applications/sort.py
@@ -1,31 +1,39 @@
-from collections import deque
+import sys
+from typing import Deque, List
 
-from .application import Application
-
+from .application import Application, ApplicationError
 
 class Sort(Application):
+    name = 'sort'
 
-    name = "sort"
+    def exec(
+        self,
+        args: List[str],
+        input: List[str],
+        output: Deque[str]
+    ) -> None:
+        reverse = '-r' in args
 
-    def __init__(self) -> None:
-        super().__init__()
-
-    def exec(self, args: list[str], input: list[str], out: deque[str]) -> None:
-        reverse_order = '-r' in args
-        file_name = None
-
+        # Separate options from potential file path
+        file_path = None
         for arg in args:
             if arg != '-r':
-                file_name = arg
+                file_path = arg
                 break
 
-        lines = input
-        if file_name:
-            with open(file_name, 'r') as file:
-                lines = file.readlines()
+        # Determine the source of data (file or stdin)
+        if file_path:
+            try:
+                with open(file_path, 'r') as file:
+                    lines = file.readlines()
+            except FileNotFoundError:
+                raise ApplicationError(
+                    f"{self.name}: {file_path}: Unable to read file."
+                )
+        else:
+            # Use stdin if no file is specified
+            lines = input
 
-        # Sorting the lines
-        sorted_lines = sorted(lines, reverse=reverse_order)
-
-        for line in sorted_lines:
-            out.append(line)
+        sorted_lines = sorted(lines, reverse=reverse)
+        formatted_output = ''.join(sorted_lines)
+        output.append(formatted_output)

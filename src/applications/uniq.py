780646096da56178345dfea765979386ab56bfc0
Maximus <maxabela@gmail.com>
2023-12-06 09:25:58 +0000

    update uniq

--- src/applications/uniq.py
+++ src/applications/uniq.py
@@ -1,34 +1,61 @@
-from collections import deque
+import sys
+from typing import Deque, List
 
-from .application import Application
-
+from .application import Application, ApplicationError
 
 class Uniq(Application):
+    name = 'uniq'
 
-    name = "uniq"
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    def exec(self, args: list[str], input: list[str], out: deque[str]) -> None:
+    def exec(
+        self,
+        args: List[str],
+        input: List[str],
+        output: Deque[str]
+    ) -> None:
         ignore_case = '-i' in args
-        file_name = None
+        file_path = None
 
+        # Check for file argument
         for arg in args:
-            if arg != '-i':
-                file_name = arg
+            if not arg.startswith('-'):
+                file_path = arg
                 break
 
-        lines = input
-        if file_name:
-            with open(file_name, 'r') as file:
-                lines = file.readlines()
+        lines = []
+        try:
+            # Read from file or stdin
+            if file_path:
+                with open(file_path, 'r') as file:
+                    lines = file.readlines()
+            else:
+                lines = input
 
-        last_line = None
-        for line in lines:
-            line_to_compare = line.lower() if ignore_case else line
-            if line_to_compare != last_line:
-                out.append(line)
-                last_line = line_to_compare
+            self.process_lines(lines, ignore_case, output)
+        except FileNotFoundError:
+            raise ApplicationError(
+                f"{self.name}: {file_path}: No such file or directory."
+            )
+        except Exception as e:
+            raise ApplicationError(str(e))
 
-# Add additional documentation or help messages as needed
+    def process_lines(
+        self,
+        lines: List[str],
+        ignore_case: bool,
+        output: Deque[str]
+    ) -> None:
+        if not lines:
+            return
+
+        previous_line = lines[0]
+        output.append(previous_line)
+
+        for line in lines[1:]:
+            if ignore_case:
+                if previous_line.strip().lower() != line.strip().lower():
+                    output.append(line)
+                    previous_line = line
+            else:
+                if previous_line.strip() != line.strip():
+                    output.append(line)
+                    previous_line = line

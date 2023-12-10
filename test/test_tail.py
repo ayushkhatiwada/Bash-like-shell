1e6c244efd102f0fe168cca527e6066dfeeddda4
Max <maxabela@gmail.com>
2023-12-10 20:16:38 +0000

    coverage for testing tail rewritten after fixing for system tests

--- test/test_tail.py
+++ test/test_tail.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import unittest
 from collections import deque
 
@@ -12,42 +11,38 @@ class TestTail(unittest.TestCase):
     tail_app = Tail()
 
     def setUp(self):
-        """
-        File Content
-        ------------
-        test_file.txt
-            Line 1
-            Line 2
-            Line 3
-            Line 4
-            Line 5
-        """
-        # Create the test file with specified content
         with open(self.TEST_FILE, 'w') as file:
             for i in range(1, 6):
                 file.write(f"Line {i}\n")
 
     def tearDown(self):
-        # Clean up the test file
         os.remove(self.TEST_FILE)
 
-    def test_tail_default_lines(self):
+    def test_tail_default_behavior(self):
         output = deque()
         self.tail_app.exec([self.TEST_FILE], [], output)
-        result = output.popleft()
+        result = output.popleft().strip()  # Stripping the newline character
+        expected = "Line 1\nLine 2\nLine 3\nLine 4\nLine 5"
+        self.assertEqual(result, expected)
 
-        self.assertIn('Line 1', result)
-        self.assertIn('Line 5', result)
+    def test_tail_n0(self):
+        output = deque()
+        self.tail_app.exec(['-n', '0', self.TEST_FILE], [], output)
+        self.assertEqual(len(output), 0)
+
+    def test_tail_more_lines_than_file_contains(self):
+        output = deque()
+        self.tail_app.exec(['-n', '10', self.TEST_FILE], [], output)
+        result = output.popleft().strip()
+        expected = "Line 1\nLine 2\nLine 3\nLine 4\nLine 5"
+        self.assertEqual(result, expected)
 
     def test_tail_custom_lines(self):
         output = deque()
-        self.tail_app.exec(['-n3', self.TEST_FILE], [], output)
-        result = output.popleft()
-
-        self.assertNotIn('Line 2', result)
-        self.assertIn('Line 3', result)
-        self.assertIn('Line 4', result)
-        self.assertIn('Line 5', result)
+        self.tail_app.exec(['-n', '3', self.TEST_FILE], [], output)
+        result = output.popleft().strip()
+        expected = "Line 3\nLine 4\nLine 5"
+        self.assertEqual(result, expected)
 
     def test_tail_file_not_found_error(self):
         with self.assertRaises(ApplicationError):
@@ -55,14 +50,20 @@ class TestTail(unittest.TestCase):
 
     def test_tail_invalid_lines_argument(self):
         with self.assertRaises(ApplicationError):
-            self.tail_app.exec(['-nabc', self.TEST_FILE], [], deque())
+            self.tail_app.exec(['-n', 'abc', self.TEST_FILE], [], deque())
 
     def test_tail_from_stdin(self):
-        input_data = deque(["Stdin Line 1\n", "Stdin Line 2\n", "Stdin Line 3\n"])
+        input_data = ["Stdin Line 1\n", "Stdin Line 2\n", "Stdin Line 3\n"]
         output = deque()
-        self.tail_app.exec(['-n2'], input_data, output)
-        result = output.popleft()
+        self.tail_app.exec(['-n', '2'], input_data, output)
+        result = output.popleft().strip()
+        expected = "Stdin Line 2\nStdin Line 3"
+        self.assertEqual(result, expected)
 
-        self.assertNotIn('Stdin Line 1', result)
-        self.assertIn('Stdin Line 2', result)
-        self.assertIn('Stdin Line 3', result)
+    def test_tail_unexpected_error(self):
+        bad_input = 123
+
+        with self.assertRaises(ApplicationError):
+            self.tail_app.exec([], bad_input, deque())
+
+

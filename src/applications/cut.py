90a7e9d0ca18375e65847b8ab05f6fbf03aaf108
Max <maxabela@gmail.com>
2023-12-10 19:36:14 +0000

    fixing cut for system tests

--- src/applications/cut.py
+++ src/applications/cut.py
@@ -74,8 +74,17 @@ class Cut(Application):
         return slices
 
     def select_bytes(self, line: str, byte_positions: List[slice]) -> str:
-        selected_bytes = []
+        # Create a set of all byte positions to be selected
+        byte_indices = set()
         for position in byte_positions:
-            selected_bytes.append(line[position])
+            start = position.start if position.start is not None else 0
+            stop = position.stop if position.stop is not None else len(line)
+            byte_indices.update(range(start, stop))
+
+        # Select bytes from the line based on the calculated indices
+        selected_bytes = [line[i] for i in sorted(byte_indices) if i < len(line)]
+
         # Join the selected bytes and remove any trailing newline
         return ''.join(selected_bytes).rstrip('\n')
+
+

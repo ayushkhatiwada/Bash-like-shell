9efac8e45525c8ae4a2675006c2315b62235162a
Max <maxabela@gmail.com>
2023-12-11 23:45:59 +0000

    converter coverage 95 babey

--- test/test_converter.py
+++ test/test_converter.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import Mock
+
 
 from antlr4 import InputStream, CommonTokenStream
 from antlr.ShellGrammarLexer import ShellGrammarLexer
@@ -16,6 +18,7 @@ from expressions import (
     Quoted,
     DoubleQuoted,
     BackQuoted,
+    SingleQuoted,
 )
 
 
@@ -109,4 +112,165 @@ class TestShellCommands(unittest.TestCase):
                 Call((Argument("echo")), Atom(Argument(Quoted(
                     BackQuoted("date")))))
             ),
+        )
+
+    def test_echo_single_quoted(self):
+        self.check_command(
+            "echo 'hello world'",
+            Command(
+                Call(
+                    Argument("echo"),
+                    Atom(Argument(Quoted(SingleQuoted('hello', ' ', 'world'))))
+                )
+            )
+        )
+
+    def test_double_quoted_with_back_quoted(self):
+        self.check_command(
+            'echo "text before `date`"',
+            Command(
+                Call(
+                    Argument("echo"),
+                    Atom(
+                        Argument(
+                            Quoted(
+                                DoubleQuoted(
+                                    "text before ",
+                                    BackQuoted("date")
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+    def test_visitQuoted_with_invalid_context(self):
+        mock_quoted_context = Mock(spec=ShellGrammarParser.QuotedContext)
+
+        mock_quoted_context.children = [Mock()]
+
+        converter = Converter()
+
+        with self.assertRaises(AssertionError):
+            converter.visitQuoted(mock_quoted_context)
+
+    def test_visitRedirection_with_invalid_context(self):
+        mock_redirection_context = Mock(spec=ShellGrammarParser.RedirectionContext)
+        mock_redirection_context.children = [Mock()]
+
+        converter = Converter()
+
+        with self.assertRaises(AssertionError):
+            converter.visitRedirection(mock_redirection_context)
+
+    def test_visitSeq_with_invalid_context(self):
+        mock_seq_context = Mock(spec=ShellGrammarParser.SeqContext)
+        mock_seq_context.children = [Mock()]
+
+        converter = Converter()
+
+        with self.assertRaises(AssertionError):
+            converter.visitSeq(mock_seq_context)
+
+    def test_visitPipe_with_invalid_context(self):
+        mock_pipe_context = Mock(spec=ShellGrammarParser.PipeContext)
+        mock_pipe_context.children = [Mock(), Mock(), Mock()]
+
+        converter = Converter()
+
+        with self.assertRaises(TypeError):  # or another appropriate error
+            converter.visitPipe(mock_pipe_context)
+
+    def test_visitCommand_with_invalid_context(self):
+        mock_command_context = Mock(spec=ShellGrammarParser.CommandContext)
+        mock_command_context.children = [Mock()]
+
+        converter = Converter()
+
+        with self.assertRaises(AssertionError):
+            converter.visitCommand(mock_command_context)
+
+    def test_seq_with_call_on_left(self):
+        self.check_command(
+            "echo hello ; ls -l",
+            Command(
+                Seq(
+                    Call(Argument("echo"), Atom(Argument("hello"))),
+                    Call(Argument("ls"), Atom(Argument("-l")))
+                )
+            )
+        )
+
+    def test_seq_with_pipe_on_right(self):
+        self.check_command(
+            "ls -l ; grep pattern | sort",
+            Command(
+                Seq(
+                    Call(Argument("ls"), Atom(Argument("-l"))),
+                    Pipe(
+                        Call(Argument("grep"), Atom(Argument("pattern"))),
+                        Call(Argument("sort"))
+                    )
+                )
+            )
+        )
+
+    def test_seq_with_pipe_on_left(self):
+        self.check_command(
+            "cat file.txt | grep pattern ; ls -l",
+            Command(
+                Seq(
+                    Pipe(
+                        Call(Argument("cat"), Atom(Argument("file.txt"))),
+                        Call(Argument("grep"), Atom(Argument("pattern")))
+                    ),
+                    Call(Argument("ls"), Atom(Argument("-l")))
+                )
+            )
+        )
+
+    def test_seq_with_nested_seq_on_right(self):
+        self.check_command(
+            "echo start ; echo middle ; echo end",
+            Command(
+                Seq(
+                    Call(Argument("echo"), Atom(Argument("start"))),
+                    Seq(
+                        Call(Argument("echo"), Atom(Argument("middle"))),
+                        Call(Argument("echo"), Atom(Argument("end")))
+                    )
+                )
+            )
+        )
+
+
+
+    def test_seq_with_invalid_right(self):
+        with self.assertRaises(AssertionError):
+            self.check_command("echo hello ; invalid_command", None)
+
+    def test_pipe_with_right_side_pipe(self):
+        self.check_command(
+            "cat file.txt | grep pattern | sort",
+            Command(
+                Pipe(
+                    Call(Argument("cat"), Atom(Argument("file.txt"))),
+                    Pipe(
+                        Call(Argument("grep"), Atom(Argument("pattern"))),
+                        Call(Argument("sort"))
+                    )
+                )
+            )
+        )
+
+    def test_call_with_direct_redirection(self):
+        self.check_command(
+            "command > output.txt",
+            Command(
+                Call(
+                    Argument("command"),
+                    Atom(Redirection(">", Argument("output.txt")))
+                )
+            )
         )
